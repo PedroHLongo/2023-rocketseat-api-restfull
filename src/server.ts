0c745762577a19e22f929f2747056985200e27f6
PedroHLongo <pedro.longo@bradescoseguros.com.br>
2023-02-18 15:01:58 -0300

    Second and third module

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import fastify from 'fastify'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.register(cookie)
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
